2609a243031e5b48e485215830d719962ab763a7
Peter Schneider <peter.schneider.127@googlemail.com>
2014-12-02 23:07:13 +0100

    2D-vectors seems ok now

--- vec32.go
+++ vec32.go
@@ -1,34 +1,87 @@
 package vec32
 
+// Basic Vector algebra
+//
+// Adding, Substracting, etc. Just a basic (stupid) implementation at
+// the moment
+
 import (
 	"fmt"
 	"math"
 )
 
+// Abstract Vector type.
+//
+// This interface encapsulate common functionality of a vector. The result
+// have to be a scalar as the cross product of two 3D-vectors is a 3D-vector
+// again and not an abstract base type anymore.
 type Vector interface {
+	// Get the dimension (2, 3, ....)
 	Dim() int
+	// The (euklidian) length
 	Length() float32
+	// The (euklidian) length squared (no square root, prefere this)
 	LengthSq() float32
 }
 
+// A two dimensional Vector
 type Vec2 struct {
 	X, Y float32
 }
 
-func (v *Vec2) Dim() int { return 2 }
+// Dimension of the vector
+func (v Vec2) Dim() int { return 2 }
 
-func (v *Vec2) Length() float32 {
+// The euklidian length
+func (v Vec2) Length() float32 {
 	return float32(math.Sqrt(float64(v.X*v.X + v.Y*v.Y)))
 }
 
-func (v *Vec2) LengthSq() float32 {
+// The euklidian length squared
+func (v Vec2) LengthSq() float32 {
 	return v.X*v.X + v.Y*v.Y
 }
 
-func (v *Vec2) String() string {
+// string representation (octave style)
+func (v Vec2) String() string {
 	return fmt.Sprintf("[%g; %g]", v.X, v.Y)
 }
 
-func (v *Vec2) Dot(v2 *Vec2) float32 {
+// Dot product
+func (v Vec2) Dot(v2 Vec2) float32 {
 	return v.X*v2.X + v.Y*v2.Y
+}
+
+// Add two vectors
+func (v Vec2) Add(v2 Vec2) Vec2 {
+	return Vec2{v.X + v2.X, v.Y + v2.Y}
+}
+
+// Substract two vectors
+func (v Vec2) Sub(v2 Vec2) Vec2 {
+	return Vec2{v.X - v2.X, v.Y - v2.Y}
+}
+
+// Multiply two vectors elementwise
+func (v Vec2) Mul(v2 Vec2) Vec2 {
+	return Vec2{v.X * v2.X, v.Y * v2.Y}
+}
+
+// get the normalized vector ( |v| == 1 )
+func (v Vec2) Normalize() Vec2 {
+	n := v.Length()
+	return Vec2{v.X / n, v.Y / n}
+}
+
+// Scale a vector by a factor
+func (v Vec2) Scale(s float32) Vec2 {
+	return Vec2{v.X * s, v.Y * s}
+}
+
+// f32-fabs
+func Abs(v float32) float32 {
+	if v >= 0 {
+		return v
+	}
+	return -v
 }
--- vec32_test.go
+++ vec32_test.go
@@ -1,14 +1,29 @@
 package vec32
 
 import (
+	"math"
 	"testing"
 )
 
 func TestVec2(t *testing.T) {
-	v := &Vec2{3, 4}
+	v := Vec2{3, 4}
+	v2 := Vec2{5, 6}
 	testVec(t, v, 2, 5.0)
+	testFloat(t, "X", 3, v.X)
+	testFloat(t, "Y", 4, v.Y)
 	testString(t, "String()", "[3; 4]", v.String())
-	testFloat(t, "Dot()", 3*5+4*6, v.Dot(&Vec2{5, 6}))
+	testFloat(t, "Dot()", 3*5+4*6, v.Dot(v2))
+	testVec2(t, "Add()", Vec2{8, 10}, v.Add(v2))
+	testVec2(t, "Sub()", Vec2{-2, -2}, v.Sub(v2))
+	testVec2(t, "Normalize()", Vec2{3.0 / 5.0, 4.0 / 5.0}, v.Normalize())
+	testVec2(t, "Scale()", Vec2{6, 8}, v.Scale(2))
+	testVec2(t, "Mul()", Vec2{15, 24}, v.Mul(v2))
+}
+
+func TestNormNaN(t *testing.T) {
+	v2 := Vec2{}.Normalize()
+	testNaN(t, "X", v2.X)
+	testNaN(t, "Y", v2.Y)
 }
 
 func testVec(t *testing.T, v Vector, dim int, length float32) {
@@ -26,5 +41,18 @@ func testFloat(t *testing.T, name string, exp, cur float32) {
 func testString(t *testing.T, name, exp, cur string) {
 	if exp != cur {
 		t.Errorf("%s is wrong - expected %s got %s", name, exp, cur)
+	}
+}
+
+func testVec2(t *testing.T, name string, exp, cur Vec2) {
+	d := exp.Sub(cur).LengthSq() / exp.LengthSq()
+	if Abs(d) > 1e-6 {
+		t.Errorf("%s is wrong - expected %s got %s", name, exp, cur)
+	}
+}
+
+func testNaN(t *testing.T, name string, val float32) {
+	if !math.IsNaN(float64(val)) {
+		t.Errorf("%s is wrong - %g is not NaN", val)
 	}
 }
