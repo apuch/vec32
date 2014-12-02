07e76c9955a5ebaee67330b5d448551a50cbe859
Peter Schneider <peter.schneider.127@googlemail.com>
2014-12-02 22:15:20 +0100

    Make X and Y globaly accessable

--- vec32.go
+++ vec32.go
@@ -12,23 +12,23 @@ type Vector interface {
 }
 
 type Vec2 struct {
-	x, y float32
+	X, Y float32
 }
 
 func (v *Vec2) Dim() int { return 2 }
 
 func (v *Vec2) Length() float32 {
-	return float32(math.Sqrt(float64(v.x*v.x + v.y*v.y)))
+	return float32(math.Sqrt(float64(v.X*v.X + v.Y*v.Y)))
 }
 
 func (v *Vec2) LengthSq() float32 {
-	return v.x*v.x + v.y*v.y
+	return v.X*v.X + v.Y*v.Y
 }
 
 func (v *Vec2) String() string {
-	return fmt.Sprintf("[%g; %g]", v.x, v.y)
+	return fmt.Sprintf("[%g; %g]", v.X, v.Y)
 }
 
 func (v *Vec2) Dot(v2 *Vec2) float32 {
-	return v.x*v2.x + v.y*v2.y
+	return v.X*v2.X + v.Y*v2.Y
 }
