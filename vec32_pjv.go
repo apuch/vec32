55bfa1a46ea2432e702dae43f700a7b985d4e602
Peter Schneider <peter.schneider.127@googlemail.com>
2015-03-19 22:32:19 +0100

    Basic header parsing - work in progress

--- vec32_pjv.go
+++ vec32_pjv.go
@@ -2,32 +2,140 @@ package vec32
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strings"
 )
 
 type Mesh struct {
 	Points []Vec3
 }
 
+const (
+	typeIgnore = iota
+	typeChar   = iota
+	typeUchar  = iota
+	typeShort  = iota
+	typeUshort = iota
+	typeInt    = iota
+	typeUint   = iota
+	typeFloat  = iota
+	typeDouble = iota
+)
+
+var typeMap = map[string]uint{
+	"char":   typeChar,
+	"uchar":  typeUchar,
+	"short":  typeUshort,
+	"int":    typeInt,
+	"uint":   typeUint,
+	"float":  typeFloat,
+	"double": typeDouble,
+}
+
+type meshBuilder struct {
+	rd         *bufio.Reader
+	mesh       *Mesh
+	facesPhase bool
+	nVerts     int
+	nFaces     int
+	vertFmt    string
+	faceFmt    string
+	vertProp   []property
+	faceProp   []property
+}
+
+type property struct {
+	dataType uint
+	Name     string
+}
+
 func ReadPLY(r io.Reader) (m *Mesh, err error) {
 	rd := bufio.NewReader(r)
+	mb := meshBuilder{}
+	mb.rd = rd
+	mb.mesh = &Mesh{}
+	mb.vertFmt = ""
+	mb.faceFmt = ""
+	mb.vertProp = make([]property, 64)
+	mb.faceProp = make([]property, 64)
+	err = mb.readHeader()
+	if err != nil {
+		return nil, err
+	}
+	return mb.mesh, nil
+}
 
-	var line string
-	line, err = rd.ReadString('\r')
+func (mb *meshBuilder) readHeader() error {
+	line, err := mb.rd.ReadString('\r')
 	if err != nil {
-		return nil, newErrorMesh(err.Error())
+		return newErrorMesh(err.Error())
 	}
-	if Strcmp(line, "ply\r") != 0 {
-		return nil, newErrorMesh("expected file-magic ply\\r")
+	if line != "ply\r" {
+		return newErrorMesh("expected file-magic ply\\r")
 	}
-	line, err = rd.ReadString('\r')
+	line, err = mb.rd.ReadString('\r')
 	if err != nil {
-		return nil, newErrorMesh(err.Error())
+		return newErrorMesh(err.Error())
 	}
 	if line != "format ascii 1.0\r" {
-		return nil, newErrorMesh("only format ascii 1.0 supported")
+		return newErrorMesh("only format ascii 1.0 supported")
 	}
-	return nil, nil
+	for {
+		line, err = mb.rd.ReadString('\r')
+		if err != nil {
+			return newErrorMesh(err.Error())
+		}
+		if line == "end_header\r" {
+			return nil
+		} else if strings.HasPrefix(line, "comment ") {
+			// pass
+		} else if strings.HasPrefix(line, "element vertex ") {
+			if e := mb.readElement(line); e != nil {
+				return e
+			}
+		} else if strings.HasPrefix(line, "element face ") {
+			if e := mb.readFace(line); e != nil {
+				return e
+			}
+		} else if strings.HasPrefix(line, "property ") {
+			if e := mb.readProperty(line); e != nil {
+				return e
+			}
+		} else {
+			return newErrorMesh("unexpected line in header: " + strings.TrimRight(line, "\r"))
+		}
+	}
+}
+
+func (mb *meshBuilder) readFace(line string) error {
+	if n, e := fmt.Sscanf(line, "element face %d\r", &mb.nFaces); e != nil || n != 1 {
+		return newErrorMesh("failed to parse number of faces")
+	}
+	return nil
+}
+
+func (mb *meshBuilder) readElement(line string) error {
+	if n, e := fmt.Sscanf(line, "element vertex %d\r", &mb.nVerts); e != nil || n != 1 {
+		return newErrorMesh("failed to parse number of vertices")
+	}
+	return nil
+}
+
+func (mb *meshBuilder) readProperty(line string) error {
+	var propType, name string
+	if n, e := fmt.Sscanf(line, "property %s %s", &propType, &name); n != 2 || e != nil {
+		return newErrorMesh("failed to parse: " + line)
+	}
+	_, ok := typeMap[propType]
+	if !ok {
+		return newErrorMesh("unknown property type: " + propType)
+	}
+	return nil
+}
+
+func (mb *meshBuilder) addVertProperty(propType uint, name string) {
+
 }
 
 // Error in mesh creation or something
--- vec32_pjv_test.go
+++ vec32_pjv_test.go
@@ -10,14 +10,29 @@ func newReader(s string) io.Reader {
 	return strings.NewReader(s)
 }
 
-func TestEmptyString(t *testing.T) {
+func TestHeaderBasic(t *testing.T) {
+	headerStart := "ply\r" +
+		"format ascii 1.0\r"
+	headerEnd := "end_header\r"
 	testError(t, "", "EOF")
 	testError(t, "plx\r", "expected file-magic ply\\r")
-	testError(t, "ply\rformat ascii 1.1\r", "only format ascii 1.0 supported")
+	testError(t, "ply\r"+
+		"format ascii 1.1\r", "only format ascii 1.0 supported")
+	testError(t, headerStart+"foobar baz\r", "unexpected line in header: foobar baz")
+	testError(t, headerStart+headerEnd, "")
+	testError(t, headerStart+"comment foo bar baz\r"+headerEnd, "")
+	testError(t, headerStart+"element vertex 0\r"+headerEnd, "")
+	testError(t, headerStart+"element face 0\r"+headerEnd, "")
+	testError(t, headerStart+"element vertex x\r"+headerEnd, "failed to parse number of vertices")
+	testError(t, headerStart+"element face x\r"+headerEnd, "failed to parse number of faces")
+
 }
 
 func testError(t *testing.T, mesh, errTest string) {
 	_, err := ReadPLY(newReader(mesh))
+	if err == nil && errTest == "" {
+		return
+	}
 	if err == nil || Strcmp(errTest, err.Error()) != 0 {
 		errString := "<nil>"
 		if err != nil {
